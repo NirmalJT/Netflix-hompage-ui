6a8efafc49f7c19e4e7eadab9d349976ce4d3996
NirmalJT <nirmaljyotithakuria@gmail.com>
2024-12-03 00:42:47 +0530

    add the whole project

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".jsdom").addEventListener("click", () => {
+  document.querySelector(".faqbox").style.paddingBottom = "300px";
+  document.querySelector(".faqbox").style.transition = "none";
+  document.querySelector(".faqbox").style.backgroundColor = "#271414";
+});
