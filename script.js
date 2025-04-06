9cb01d4f22136b7f3e80b21f93861f35e74a56b5
NirmalJT <nirmaljyotithakuria@gmail.com>
2025-04-06 16:19:41 +0530

    solve hero section bug

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".jsdom").addEventListener("click", () => {
-  document.querySelector(".faqbox").style.paddingBottom = "300px";
-  document.querySelector(".faqbox").style.transition = "none";
-  document.querySelector(".faqbox").style.backgroundColor = "#271414";
-});
